b2b3cea599fbcaa07f5b66d97d6e090e613662eb
Ahmed Sayed <ahm.say14@gmail.com>
2023-09-15 02:36:47 +0300

    learn useMemo and useRef proberly

--- app/page.tsx
+++ app/page.tsx
@@ -5,39 +5,59 @@ import Header from "../components/header";
 import Footer from "../components/footer";
 import RoutingButton from "@/components/routingButton";
 import styles from "../components/routingButton.module.css";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 export default function Home() {
-  const [counter, setCounter] = useState(0);
-  const [discount, setDiscount] = useState(false);
-  const [text, setText] = useState("");
+  // const [counter, setCounter] = useState(0);
+  // const [discount, setDiscount] = useState(false);
+  // const [text, setText] = useState("");
 
-  const increment = () => {
-    setCounter(counter + 1);
-    if (counter > 8) {
-      setDiscount(true);
-      setText("you have a discount with $100");
-    }
-  };
+  // const increment = () => {
+  //   setCounter(counter + 1);
+  //   if (counter > 8) {
+  //     setDiscount(true);
+  //     setText("you have a discount with $100");
+  //   }
+  // };
 
-  const decrement = () => {
-    if (counter > 0) {
-      setCounter(counter - 1);
-    }
-  };
-  const reset = () => {
-    setCounter(counter * 0);
-  };
+  // const decrement = () => {
+  //   if (counter > 0) {
+  //     setCounter(counter - 1);
+  //   }
+  // };
+  // const reset = () => {
+  //   setCounter(counter * 0);
+  // };
 
-  useEffect(() => {}, [discount]);
+  // useEffect(() => {}, [discount]);
 
   // let hasHeader = true;
+
+  const inputRef = useRef<HTMLInputElement>(null);
+  const [validate, setValidate] = useState("");
+  const [error, setError] = useState("");
+  const handleClicked = () => {
+    if (inputRef.current) {
+      if (inputRef.current.value === "ahmed") {
+        setValidate("Correct user name");
+      } else if (inputRef.current.value === "mariam") {
+        setValidate("Correct user name");
+      } else {
+        setError("Incorrect user name");
+      }
+    }
+  };
+
   return (
     <main>
+      <input ref={inputRef} type='text' />
+      <h2 style={{ color: "green" }}>{validate}</h2>
+      <h2 style={{ color: "red" }}>{error}</h2>
+      <button onClick={handleClicked}>check</button>
       {/* {hasHeader && <Header text='Hello i am a header' />}
       <h1>Hello There</h1>
       <Footer text='Hello from Footer' />
-      <RoutingButton /> */}
+      <RoutingButton /> 
       <h1>ليك خصم 100 دولار لو عديت عشر منتجات </h1>
       <h1>Counter: {counter}</h1>
       <h3 style={{ color: "red" }}>{text}</h3>
@@ -49,10 +69,10 @@ export default function Home() {
       </button>
       {/* <button onClick={incrementBy5} className={styles.btn}>
         incrementBy5
-      </button> */}
+      </button> 
       <button onClick={reset} className={styles.btn}>
         0
-      </button>
+      </button>*/}
     </main>
   );
 }
